7d330b5a3c8f6eda29e36edeec44e88f84e7a847
ShabeenaC <165435835+ShabeenaC@users.noreply.github.com>
2025-02-24 16:19:24 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 function learnMore() {
-    alert("Thank you for submitting to our form!");
+    var password="hello";
+    console.log(password);
+        alert("Thank you for submitting to our form!");
 }
